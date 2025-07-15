f879758911b6b55f1ba8c92b2dd49ed17982081f
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-07-14 21:13:22 -0300

    chore(logo): adição de estilo de transição ao componente

--- src/components/custom/Logo.tsx
+++ src/components/custom/Logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 /** Imagem da logo do site. */
 export default function Logo(
-  props: Omit<React.ComponentProps<typeof Image>, "src" | "alt"> // aceitar props de Image não definidos
+  { className, ...props }: Omit<React.ComponentProps<typeof Image>, "src" | "alt"> // aceitar props de Image não definidos
 ) {
-  return <Image src="/logo/logo-inline.svg" alt="Logo do site" width={312} height={48} {...props} />
+  return <Image src="/logo/logo-inline.svg" alt="Logo do site" width={312} height={48} className={`transition-all ${className}`} {...props} />
 }
