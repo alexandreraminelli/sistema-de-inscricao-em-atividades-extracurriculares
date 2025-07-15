b6aff46e84fd3284d4550a6e88d4fd0895a276dd
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-07-14 21:21:34 -0300

    Squashed commit: feature/login/design
    commit eb4819babe9f13f1dac638141b86547c61df4066
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Mon Jul 14 21:21:17 2025 -0300
    
        feat(login/page): remoção de imagem mobile desnecessária
    
    commit 6584b0053e45edb89ea19ff65a43d4007d28c6a3
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Mon Jul 14 21:14:44 2025 -0300
    
        chore(login/page): ajuste na responsividade
    
    commit f879758911b6b55f1ba8c92b2dd49ed17982081f
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Mon Jul 14 21:13:22 2025 -0300
    
        chore(logo): adição de estilo de transição ao componente
    
    commit 340c3f00cb8f3851e3944992e9ff957e928160a6
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Mon Jul 14 21:04:40 2025 -0300
    
        feat(shadcn): instalação dos componentes Form, Button e Label
    
    commit 60e99b3b3b8f803070c0083be93cd50ef16f78f0
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Mon Jul 14 20:59:30 2025 -0300
    
        feat(login/page): imagem responsiva para desktop
    
    commit 94f5fab52241039caa74c391811ee79a015495e2
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Mon Jul 14 20:42:05 2025 -0300
    
        feat(login/page): conteúdo da página de login
    
    commit 4af2ccca68dcbd070a2ed054c0c34bbdd17c8da0
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Mon Jul 14 19:40:29 2025 -0300
    
        feat(logo): criação do componente para a logo do site
    
    commit 0cad04640d49a4bdf160cf1286ca0daa3790823e
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Mon Jul 14 19:28:06 2025 -0300
    
        feat(background): adição da imagem de background da página de login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,47 @@
+import Logo from "@/components/custom/Logo"
+import Image from "next/image"
+
+/** Página de login. */
+export default function LoginPage() {
+  return (
+    <div
+      className="flex flex-row w-dvw h-dvh justify-between
+        py-4 md:py-3 px-2.5 md:px-3 gap-3.5"
+    >
+      {/* Conteúdo da página */}
+      <div className="flex flex-col h-full w-full md:px-14 md:py-1.5">
+        <header className="flex flex-col items-center md:items-start gap-14">
+          {/* Logo */}
+          <Logo className="h-10 md:h-11 lg:h-12" />
+
+          {/* Título e subtítulo */}
+          <div className="space-y-4 text-center md:text-start">
+            <h1 className="text-3xl sm:text-4xl font-semibold">Acesse sua conta</h1>
+            <p className="text-sm text-gray-700 dark:text-gray-300">Entre para visualizar e se inscrever em atividades extracurriculares</p>
+          </div>
+        </header>
+
+        {/* Form */}
+        <main>Formulário de login</main>
+
+        <aside className="flex flex-col items-center justify-end h-full">
+          {/* Problemas */}
+          <p className="text-sm text-center text-slate-600 dark:text-slate-400 my-2 lg:mx-2 object-cover">
+            Está tendo problemas para acessar? <br />
+            Entre em contato com o suporte da faculdade.
+          </p>
+        </aside>
+      </div>
+
+      {/* Imagem desktop */}
+      <Image
+        src="/images/login-bg.jpg"
+        alt="Fachada da universidade"
+        width={626}
+        height={812}
+        className="rounded-xl rounded-bl-4xl shadow-lg 
+        object-cover transition-all max-md:hidden max-lg:w-2/5 lg:w-1/2"
+      />
+    </div>
+  )
+}
