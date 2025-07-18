05c36d81f84a75d6915660effa55823dc6eb1325
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-07-18 17:25:28 -0300

    fix(input-password): evitar que formulário seja enviado ao clicar no botão de ocultar/mostrar senha

--- src/components/custom/form/PasswordInput.tsx
+++ src/components/custom/form/PasswordInput.tsx
@@ -12,6 +12,13 @@ export default function PasswordInput({ className, ...props }: React.ComponentPr
   /** Rótulo do botão, dependendo da visibilidade atual. */
   const label = isView ? "Ocultar senha" : "Mostrar senha"
 
+  /** Função para alternar a visibilidade da senha. */
+  const handleToggleVisibility = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault() // Prevenir o comportamento padrão (enviar form)
+    e.stopPropagation() // Evitar a propagação para outros elementos
+    setIsView(!isView) // Alternar visibilidade
+  }
+
   return (
     <div className={`flex flex-row gap-2 ${className}`}>
       <Input
@@ -20,17 +27,8 @@ export default function PasswordInput({ className, ...props }: React.ComponentPr
       />
       {/* Botão de ocultar/mostrar senha */}
       <Tooltip>
-        <TooltipTrigger>
-          <Button
-            type="button"
-            variant="outline"
-            size="icon"
-            onClick={() => {
-              // Alternar visibilidade
-              setIsView(!isView)
-            }}
-            aria-label={label}
-          >
+        <TooltipTrigger asChild>
+          <Button type="button" variant="outline" size="icon" onClick={handleToggleVisibility} aria-label={label}>
             {isView ? (
               // Ícone de ocultar senha
               <EyeIcon className="" />
