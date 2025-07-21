56d09ab6a3cd97720115175e2874bd5def5dd188
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-07-21 18:25:49 -0300

    feat(sidebar): manter o estado da sidebar ao navegar entre páginas

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,20 +1,26 @@
 import AppSidebar from "@/components/layout/sidebar/AppSidebar"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { getServerSession } from "next-auth"
+import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 
 /** Layout dos componentes principais da aplicação. */
-export default async function Layout({ children }: { children: React.ReactNode }) {
-  // Redirecionar usuários não autenticados para a página de login
+export default async function AppLayout({ children }: { children: React.ReactNode }) {
+  /** Sessão do usuário. */
   const session = await getServerSession()
-  if (!session) redirect("/login")
+  if (!session) redirect("/login") // Redirecionar usuários não autenticados para a página de login
+
+  /** Interface do Next para ler cookies no servidor. */
+  const cookieStore = await cookies()
+  /** Se a sidebar está aberta ou fechada. Uso para manter o estado ao mudar de página. */
+  const defaultOpen = cookieStore.get("sidebar_state")?.value === "true"
 
   return (
-    <SidebarProvider>
+    <SidebarProvider defaultOpen={defaultOpen}>
       {/* Sidebar */}
       <AppSidebar />
 
-      <div className="flex flex-row">
+      <div>
         {/* Botão de expandir/compactar sidebar */}
         <SidebarTrigger />
         {/* Página */}
