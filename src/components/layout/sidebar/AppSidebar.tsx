5f7ecdfa0c7d3f418ab37556ec7e55bef48e94b4
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-07-21 15:22:58 -0300

    feat(sidebar): exibir ícones ao recolher a sidebar

--- src/components/layout/sidebar/AppSidebar.tsx
+++ src/components/layout/sidebar/AppSidebar.tsx
@@ -1,12 +1,14 @@
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader } from "@/components/ui/sidebar"
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarTrigger } from "@/components/ui/sidebar"
 import AppSidebarMenu from "./AppSidebarMenu"
 
 /** Sidebar do aplicativo exibido nas páginas do usuário (após autenticação). */
 export default function AppSidebar() {
   return (
-    <Sidebar variant="floating">
+    <Sidebar variant="floating" collapsible="icon">
       {/* Cabeçalho */}
-      <SidebarHeader />
+      <SidebarHeader>
+        <SidebarTrigger size="lg" />
+      </SidebarHeader>
 
       {/* Conteúdo */}
       <SidebarContent>
