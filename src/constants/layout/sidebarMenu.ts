9c673689b3095d499c690804ffe695c302724e15
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-07-24 18:31:54 -0300

    chore(sidebar): remoção de constante não utilizada

--- src/constants/layout/sidebarMenu.ts
+++ src/constants/layout/sidebarMenu.ts
@@ -6,18 +6,3 @@ export const quickAccessMenu: SidebarItemsType[] = [
   { title: "Início", role: "all", href: "/", icon: HouseIcon },
   { title: "Suas Inscrições", role: "student", href: "/inscricoes", icon: ClipboardListIcon },
 ]
-
-/** Dados dos ícones do menu. */
-export const sidebarMenu: SidebarGroupType[] = [
-  {
-    label: "Acesso Rápido",
-    menu: [],
-  },
-  {
-    label: "Explorar atividades",
-    menu: [
-      // Todas as atividades
-      // { title: "Todas as atividades", href: "/atividades", icon: BookOpenIcon },
-    ],
-  },
-]
