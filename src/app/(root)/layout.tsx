d398074df282becea2e3977b38e2593ad58142f7
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-07-25 17:05:54 -0300

    Squashed commit: feature/layout/header
    commit 1f3c56013baaf4f410dbc26d05838489765ac5ef
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Fri Jul 25 17:05:12 2025 -0300
    
        feat(header): criação de cabeçalho básico pro aplicativo
    
    commit f7417515a5246a54942dbf6860f9c292f9951f15
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Fri Jul 25 16:55:35 2025 -0300
    
        feat(sidebar/trigger): aumento do botão de abrir/fechar a sidebar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,6 @@
+import Header from "@/components/layout/Header"
 import AppSidebar from "@/components/layout/sidebar/AppSidebar"
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { SidebarProvider } from "@/components/ui/sidebar"
 import { getServerSession } from "next-auth"
 import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
@@ -20,9 +21,10 @@ export default async function AppLayout({ children }: { children: React.ReactNod
       {/* Sidebar */}
       <AppSidebar />
 
-      <div className="m-2">
+      <div className="m-2 w-full">
         {/* Botão de expandir/compactar sidebar */}
-        <SidebarTrigger />
+        <Header />
+
         {/* Página */}
         {children}
       </div>
