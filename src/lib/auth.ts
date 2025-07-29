76b5e43dc4a40977ef0977d61f00e055ff2595a3
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-07-29 18:38:55 -0300

    refactor(auth): configurações do NextAuth em arquivo separado para reutilização

---
+++ src/lib/auth.ts
@@ -0,0 +1,66 @@
+/* Configurações do NextAuth. */
+
+import { db } from "@/database/drizzle"
+import { users } from "@/database/schema"
+import { UserRole } from "@/types/auth/authCredentials"
+import { compare } from "bcryptjs"
+import { eq } from "drizzle-orm"
+import { NextAuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) return null
+
+        const userResponse = await db.select().from(users).where(eq(users.email, credentials.email.toString())).limit(1)
+
+        if (userResponse.length === 0) return null
+        const user = userResponse[0]
+
+        const isPasswordValid = await compare(credentials.password.toString(), user.password)
+
+        if (!isPasswordValid) return null
+
+        return {
+          id: user.id.toString(),
+          email: user.email.toString(),
+          name: user.name.toString(),
+          role: user.role as UserRole,
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+        token.email = user.email
+        token.name = user.name
+        token.role = user.role as UserRole
+      }
+      return token
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string
+        session.user.name = token.name as string
+        session.user.email = token.email as string
+        session.user.role = token.role as UserRole
+      }
+      return session
+    },
+  },
+}
