2f119cdbf6dc0ea0945f83ebc2e5fa99f6e15498
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-07-30 17:34:55 -0300

    feat(home/cards): estilização para mobile

--- src/components/custom/home/HeroCards.tsx
+++ src/components/custom/home/HeroCards.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { CardLinkType } from "@/types/content/CardType"
 import Image from "next/image"
 import Link from "next/link"
@@ -12,27 +12,30 @@ interface Props {
 /** Cards de Hero Section. */
 export default function HeroCards({ cards }: Props) {
   return (
-    <section>
+    <section
+      className="flex
+        flex-col gap-4
+        md:flex-row"
+    >
       {cards.map((card) => (
         <Card key={card.href}>
-          {/* Conteúdo do card */}
-          <div>
-            {/* Cabeçalho */}
-            <CardHeader>
-              <CardTitle>{card.title}</CardTitle>
-              <CardDescription>{card.description}</CardDescription>
-            </CardHeader>
-            {/* Botão */}
-            <CardContent>
-              <Button asChild>
-                <Link href={card.href}>{card.buttonText}</Link>
-              </Button>
-            </CardContent>
-          </div>
-          {/* Imagem */}
-          <aside>
-            <Image src={card.image} alt={`${card.title} icon`} width={150} height={112} className="object-contain" />
-          </aside>
+          {/* Cabeçalho */}
+          <CardHeader className="text-center">
+            <CardTitle>{card.title}</CardTitle>
+            <CardDescription>{card.description}</CardDescription>
+          </CardHeader>
+          {/* Ilustração */}
+          <CardContent className="mx-auto">
+            <aside>
+              <Image src={card.image} alt={`${card.title} icon`} width={150} height={112} className="object-contain max-h-40" />
+            </aside>
+          </CardContent>
+          {/* Botão */}
+          <CardFooter className="mx-auto">
+            <Button asChild>
+              <Link href={card.href}>{card.buttonText}</Link>
+            </Button>
+          </CardFooter>
         </Card>
       ))}
     </section>
