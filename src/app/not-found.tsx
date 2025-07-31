87741dbad2a85d481e56e56fb25ba12a4c5c58fc
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-07-31 20:07:43 -0300

    feat(404): atualização da página 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,38 +1,38 @@
 import Logo from "@/components/custom/Logo"
 import { Button } from "@/components/ui/button"
-import { HomeIcon, SearchXIcon } from "lucide-react"
+import { HomeIcon } from "lucide-react"
+import Image from "next/image"
 import Link from "next/link"
 
 /** Página de erro 404 (não encontrado). */
 export default function NotFound() {
   return (
     <div
-      className="h-dvh flex flex-col justify-between items-center 
-        gap-4 md:gap-9 px-4 md:px-24 pt-20 pb-6"
+      className="h-full flex flex-col items-center 
+        gap-4 px-4 pt-8 pb-6
+        md:gap-6 md:px-24"
     >
-      <main
-        className="flex flex-col items-center
-          gap-10"
-      >
-        {/* Título */}
-        <div className="flex flex-row items-center gap-1.5 lg:gap-3.5">
-          <SearchXIcon className="size-16 lg:size-20" />
-          <h1 className="font-semibold text-3xl md:text-4xl lg:text-5xl transition-all">404: Página Não Encontrada</h1>
-        </div>
+      {/* Imagem */}
+      <aside>
+        <Image src="/images/illustrations/404.svg" alt="Ilustração de cachorro olhando triste após comer a página" width={185} height={216} />
+      </aside>
+      {/* Título */}
+      <header>
+        <h1 className="font-semibold text-center text-3xl lg:text-4xl">404: Página Não Encontrada</h1>
+      </header>
+      <main className="text-center space-y-2 text-base text-muted-foreground">
         {/* Explicação */}
-        <div className="text-center space-y-2 *:text-base lg:*:text-lg">
-          <p>A página que você está tentando acessar não existe ou não é possível acessá-la no momento.</p>
-          <p>Verifique se o endereço inserido está correto ou tente novamente mais tarde.</p>
-        </div>
+        <p>A página que você está tentando acessar não existe ou não é possível acessá-la no momento.</p>
+        <p>Verifique se o endereço inserido está correto ou tente novamente mais tarde.</p>
+
         {/* Botão pro início */}
-        <Button size="lg" className="w-fit">
+        <Button size="lg" className="mt-2 md:mt-4 w-fit">
           <HomeIcon />
           <Link href="/">Voltar pro início</Link>
         </Button>
       </main>
-
       {/* Rodapé */}
-      <footer>
+      <footer className="mt-auto">
         <Logo className="h-10 md:h-12" />
       </footer>
     </div>
