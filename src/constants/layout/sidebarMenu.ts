67d191e1a4a3606952fbbdcbfcbec5359fabeed0
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-07-31 17:10:36 -0300

    feat(sidebar/menu): definição dos links para os alunos

--- src/constants/layout/sidebarMenu.ts
+++ src/constants/layout/sidebarMenu.ts
@@ -1,11 +1,14 @@
 import { SidebarItemsType } from "@/types/layout/SidebarMenuType"
-import { BookOpenIcon, ClipboardListIcon, HouseIcon } from "lucide-react"
+import { BookOpenIcon, CalendarIcon, ClipboardListIcon, GraduationCapIcon, HouseIcon, InfoIcon } from "lucide-react"
 
 /** Menu da sidebar para alunos. */
 export const studentSidebar: SidebarItemsType[] = [
   { label: "Início", href: "/", icon: HouseIcon },
   { label: "Atividades", href: "/atividades", icon: BookOpenIcon },
   { label: "Suas Inscrições", href: "/inscricoes", icon: ClipboardListIcon },
+  { label: "Calendário", href: "/calendario", icon: CalendarIcon },
+  { label: "Certificados", href: "/certificados", icon: GraduationCapIcon },
+  { label: "Informações Gerais", href: "/informacoes", icon: InfoIcon },
 ]
 
 /** Menu da sidebar para professores e administradores. */
