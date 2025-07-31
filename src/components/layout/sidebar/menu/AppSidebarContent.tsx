7458b65aef33921938227de54105d5a0f76f9132
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-07-31 17:01:54 -0300

    feat(sidebar/menu): simplificação do menu da sidebar

--- src/components/layout/sidebar/menu/AppSidebarContent.tsx
+++ src/components/layout/sidebar/menu/AppSidebarContent.tsx
@@ -1,104 +1,49 @@
 "use client"
 
-import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible"
-import { SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarMenuSub, SidebarMenuSubItem } from "@/components/ui/sidebar"
-import { quickAccessMenu } from "@/constants/layout/sidebarMenu"
+import { SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
+import { studentSidebar, teacherSidebar } from "@/constants/layout/sidebarMenu"
 import { UserRole } from "@/types/auth/authCredentials"
-import { SidebarItemsType } from "@/types/layout/SidebarMenuType"
-import { ChevronRight, Grid2x2Icon, LinkIcon, LucideIcon } from "lucide-react"
+import { LinkIcon } from "lucide-react"
 import { useSession } from "next-auth/react"
 import { usePathname } from "next/navigation"
-import { useState } from "react"
 
 /** Conteúdo principal do Sidebar */
 export default function AppSidebarContent() {
   // Obter role do usuário
   const { data: session } = useSession()
-  const userRole = session?.user?.role
+  const userRole: UserRole = session?.user?.role
+  /** Itens do menu. Itens variam de acordo com o papel do usuário. */
+  const items = userRole === "student" ? studentSidebar : teacherSidebar
+
+  /** Obter pathname atual. */
+  const pathname = usePathname()
 
   return (
     <SidebarContent>
-      {/* Acesso Rápido */}
-      <AppSidebarGroup label="Acesso Rápido">
-        <AppSidebarMenu itemList={quickAccessMenu} userRole={userRole} />
-      </AppSidebarGroup>
-      {/* Pesquisar atividades */}
-      <AppSidebarGroup label="Pesquisar atividades">
-        {/* Categorias */}
-        <AppSidebarCollapsibleMenu title="Categorias" Icon={Grid2x2Icon} />
-      </AppSidebarGroup>
+      <SidebarGroup>
+        {/* Título */}
+        <SidebarGroupLabel>Inscrição em Atividades Extracurriculares</SidebarGroupLabel>
+        {/* Menu */}
+        <SidebarGroupContent>
+          <SidebarMenu>
+            {items.map((item) => (
+              <SidebarMenuItem key={item.href}>
+                <SidebarMenuButton
+                  asChild
+                  isActive={pathname === item.href} // se link está ativo
+                  aria-label={`Link para ${item.title}`}
+                  tooltip={item.title} // tooltip para sidebar recolhida
+                >
+                  <a href={item.href}>
+                    {item.icon ? <item.icon /> : <LinkIcon />} {/* Ícone do item */}
+                    <span>{item.title}</span> {/* Título do item */}
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
     </SidebarContent>
-  )
-}
-
-/** Grupo do sidebar. */
-function AppSidebarGroup({ label, children }: { label: string; children?: React.ReactNode }) {
-  return (
-    <SidebarGroup>
-      <SidebarGroupLabel>{label}</SidebarGroupLabel>
-      <SidebarGroupContent>{children}</SidebarGroupContent>
-    </SidebarGroup>
-  )
-}
-
-/** Menu do Sidebar.  */
-function AppSidebarMenu({ itemList, userRole }: { itemList: SidebarItemsType[]; userRole: UserRole }) {
-  // Obter pathname atual
-  const pathname = usePathname()
-
-  // Filtrar itens de acordo com o tipo de usuário
-  itemList = itemList.filter((item) => item.role === "all" || item.role === userRole)
-
-  return (
-    <SidebarMenu>
-      {itemList.map((item) => (
-        <SidebarMenuItem key={item.href}>
-          <SidebarMenuButton
-            asChild
-            isActive={pathname === item.href} // se link está ativo
-            aria-label={`Link para ${item.title}`}
-            tooltip={item.title} // tooltip para sidebar recolhida
-          >
-            <a href={item.href}>
-              {item.icon ? <item.icon /> : <LinkIcon />} {/* Ícone do item */}
-              <span>{item.title}</span> {/* Título do item */}
-            </a>
-          </SidebarMenuButton>
-        </SidebarMenuItem>
-      ))}
-    </SidebarMenu>
-  )
-}
-
-/** Menu do sidebar colapsível. */
-function AppSidebarCollapsibleMenu({ title, Icon /*itemList, userRole*/ }: { title: string; Icon: LucideIcon /*itemList: SidebarItemsType[]; userRole: UserRole*/ }) {
-  // Status do collapsible (aberto ou fechado)
-  const [open, setOpen] = useState(true)
-
-  return (
-    <SidebarMenu>
-      <Collapsible defaultOpen={open} onOpenChange={setOpen} className="group/collapsible">
-        <SidebarMenuItem>
-          {/* Item principal */}
-          <CollapsibleTrigger asChild>
-            <SidebarMenuButton>
-              <Icon />
-              {title}
-              {/* Ícone de seta (animação ao expandir) */}
-              <ChevronRight className={`ms-auto transition-transform ${open ? "rotate-90" : ""}`} />
-            </SidebarMenuButton>
-          </CollapsibleTrigger>
-
-          {/* Sub-itens */}
-          <CollapsibleContent>
-            <SidebarMenuSub>
-              <SidebarMenuSubItem>Sub-item 1</SidebarMenuSubItem>
-              <SidebarMenuSubItem>Sub-item 1</SidebarMenuSubItem>
-              <SidebarMenuSubItem>Sub-item 1</SidebarMenuSubItem>
-            </SidebarMenuSub>
-          </CollapsibleContent>
-        </SidebarMenuItem>
-      </Collapsible>
-    </SidebarMenu>
   )
 }
