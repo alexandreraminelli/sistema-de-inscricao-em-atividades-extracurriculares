3fb5d8cf8c8fc2cb309b79a42e2c4b6a6c65785e
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-08-01 18:53:49 -0300

    feat(atividades): obter categorias do banco de dados

--- src/app/(root)/atividades/page.tsx
+++ src/app/(root)/atividades/page.tsx
@@ -1,4 +1,6 @@
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
+import { db } from "@/database/drizzle"
+import { category } from "@/database/schema"
 import { authOptions } from "@/lib/auth"
 import { getServerSession } from "next-auth"
 
@@ -7,6 +9,9 @@ export default async function AtividadesPage() {
   // Obter sessão do usuário
   const session = await getServerSession(authOptions)
   const userRole = session?.user?.role
+
+  // Categorias
+  const categories = await db.select().from(category).orderBy(category.name)
 
   return (
     <div>
@@ -24,12 +29,19 @@ export default async function AtividadesPage() {
       </Breadcrumb>
 
       {/* Conteúdo */}
-      <main>
+      <main className="space-y-4">
         {/* Título */}
-        <div className="space-y-1 text-center md:text-justify">
+        <header className="space-y-1 text-center md:text-justify">
           <h2 className="font-semibold text-3xl">Atividades Oferecidas</h2>
           <p className="text-sm md:text-base text-muted-foreground">Veja as atividades extracurriculares abertas para inscrição e escolha as que mais combinam com você.</p>
-        </div>
+        </header>
+
+        {/* Teste categorias */}
+        <section>
+          {categories.map((cat) => (
+            <p>{cat.name}</p>
+          ))}
+        </section>
       </main>
     </div>
   )
