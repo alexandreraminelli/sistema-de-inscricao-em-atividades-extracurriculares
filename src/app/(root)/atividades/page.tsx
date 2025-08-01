192bf1f259d4b0d52dfe30fcf955c743297bbb12
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-08-01 18:33:48 -0300

    feat(atividades): breadcrumb e título

--- src/app/(root)/atividades/page.tsx
+++ src/app/(root)/atividades/page.tsx
@@ -1,9 +1,36 @@
-/** Página de inscrições do aluno. */
-export default function AtividadesPage() {
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
+import { authOptions } from "@/lib/auth"
+import { getServerSession } from "next-auth"
+
+/** Página de atividades. */
+export default async function AtividadesPage() {
+  // Obter sessão do usuário
+  const session = await getServerSession(authOptions)
+  const userRole = session?.user?.role
+
   return (
     <div>
-      <h1 className="font-semibold text-3xl">Atividades</h1>
-      <p>Lista de atividades oferecidas.</p>
+      {/* Breadcrumb */}
+      <Breadcrumb className="my-3.5">
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Início</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Atividades Extracurriculares</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      {/* Conteúdo */}
+      <main>
+        {/* Título */}
+        <div className="space-y-1 text-center md:text-justify">
+          <h2 className="font-semibold text-3xl">Atividades Oferecidas</h2>
+          <p className="text-sm md:text-base text-muted-foreground">Veja as atividades extracurriculares abertas para inscrição e escolha as que mais combinam com você.</p>
+        </div>
+      </main>
     </div>
   )
 }
