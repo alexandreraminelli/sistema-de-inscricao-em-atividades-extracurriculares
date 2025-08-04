4abb4b1ce410298613ffb7eaccd18ebbf964fb69
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-08-04 16:27:50 -0300

    feat(activity-form): título, campos atividade e descrição e botão de enviar

--- src/components/custom/form/ActivityForm.tsx
+++ src/components/custom/form/ActivityForm.tsx
@@ -1,10 +1,14 @@
 "use client"
 
-import { activity } from "@/database/schema"
+import { Button } from "@/components/ui/button"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
 import { createActivity } from "@/lib/actions/activity"
 import { activitySchema } from "@/schemas/activitySchema"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useRouter } from "next/router"
+import { PlusIcon } from "lucide-react"
+import { useRouter } from "next/navigation"
 import { useForm } from "react-hook-form"
 import { toast } from "sonner"
 import z from "zod"
@@ -50,5 +54,79 @@ export default function ActivityForm({ type }: Props) {
     else toast.error("Erro ao criar atividade!", { description: result.message })
   }
 
-  return <p>Form de atividades extracurriculares</p>
+  return (
+    <section className="space-y-4">
+      {/* Título do formulário */}
+      <header>
+        <h1 className="font-semibold text-2xl sm:text-3xl md:text-4xl text-center md:text-start">Formulário de Atividades</h1>
+      </header>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          {/* Nome da atividade */}
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nome da atividade</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="Nome da atividade" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Categoria */}
+          <FormField
+            control={form.control}
+            name="category"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Categoria</FormLabel>
+                <FormControl></FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Descrição */}
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Descrição</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="Descrição da atividade. Objetivos, competências desenvolvidas, tópicos abordados, etc." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Aplicador */}
+          <FormField
+            control={form.control}
+            name="teacher"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Aplicador</FormLabel>
+                <FormControl></FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          {/* Imagem */}
+
+          {/* Botão de enviar */}
+          <Button type="submit" className="mt-5 max-md:w-full">
+            <PlusIcon />
+            Adicionar Atividade
+          </Button>
+        </form>
+      </Form>
+    </section>
+  )
 }
