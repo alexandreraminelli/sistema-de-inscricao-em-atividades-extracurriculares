eff1da4d6b1f20ea218bff8d951ab242ba4b9856
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-08-04 11:05:48 -0300

    feat(adicionar-atividade): redirecionar usuários que não são professores

--- src/app/(root)/atividades/adicionar/page.tsx
+++ src/app/(root)/atividades/adicionar/page.tsx
@@ -1,9 +1,17 @@
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
+import { authOptions } from "@/lib/auth"
+import { UserRole } from "@/types/auth/authCredentials"
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation"
 
 /** Página de criar atividade. */
-export default function CreateActivityPage() {
+export default async function CreateActivityPage() {
+  // Obter sessão
+  const session = await getServerSession(authOptions)
+  const userRole = session?.user?.role as UserRole
   // Redirecionar usuários que não são professores
-  
+  if (userRole !== "teacher") redirect("/atividades")
+
   return (
     <div>
       {/* Breadcrumb */}
