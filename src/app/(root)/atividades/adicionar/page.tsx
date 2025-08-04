efb99b8347ef64ccf53187a1a5d33aefd54aca85
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-08-04 18:53:56 -0300

    chore(create-activity): ajuste no tamanho da imagem

--- src/app/(root)/atividades/adicionar/page.tsx
+++ src/app/(root)/atividades/adicionar/page.tsx
@@ -35,11 +35,11 @@ export default async function CreateActivityPage() {
 
       <div className="flex flex-col md:flex-row gap-5 lg:gap-10  justify-between">
         {/* Formulário */}
-        <main className="w-full max-w-3xl md:min-w-2/3 lg:min-w-xl">
+        <main className="w-full max-w-4xl md:min-w-2/3 lg:min-w-xl">
           <ActivityForm type="create" />
         </main>
         {/* Imagem */}
-        <aside className="w-full self-center">
+        <aside className="self-center">
           <Image src="/images/illustrations/atividades.svg" alt="Ilustração de atividades" width={752} height={556} className="object-cover" />
         </aside>
       </div>
