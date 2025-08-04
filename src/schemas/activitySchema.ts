0bf3e414644321b2f67f3cde15542cd628ee578c
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-08-04 11:16:10 -0300

    feat(zod): zod schema para atividades extracurriculares

---
+++ src/schemas/activitySchema.ts
@@ -0,0 +1,15 @@
+import z from "zod"
+
+/** Zod schema para atividades extracurriculares. */
+export const activitySchema = z.object({
+  /** Nome da atividade extracurricular. */
+  name: z.string().min(5).max(100),
+  /** Categoria da atividade. */
+  category: z.uuid("Selecione uma categoria."),
+  /** Descrição da atividade. */
+  description: z.string().default("Sem descrição."),
+  /** Professor responsável por aplicar a atividade. */
+  teacher: z.uuid("Selecione um professor."),
+  /** Imagem de capa da atividade. */
+  coverImg: z.string(),
+})
