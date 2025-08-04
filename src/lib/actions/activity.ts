117ba53133b24af868779060bac68cfeed950def
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-08-04 15:26:54 -0300

    feat(actions): criação de função para criar uma atividade no banco de dados

---
+++ src/lib/actions/activity.ts
@@ -0,0 +1,22 @@
+"use server"
+
+import { db } from "@/database/drizzle"
+import { activity } from "@/database/schema"
+import { toast } from "sonner"
+
+/** Função para adicionar uma atividade extracurricular no banco de dados. */
+export async function createActivity(params: typeof activity.$inferSelect) {
+  try {
+    /** Adiciona e recupera a atividade no banco de dados. */
+    const newActivity = await db.insert(activity).values(params).returning()
+    // Retornar atividade criada
+    toast.success("Atividade criada com sucesso!", { description: `Atividade '${newActivity[0].name}' criada.` })
+    return {
+      success: true,
+      data: JSON.parse(JSON.stringify(newActivity[0])),
+    }
+  } catch (error) {
+    toast.error("Erro ao criar atividade!", { description: `${error}` })
+    return { success: false, message: "Um erro ocorreu ao criar a atividade." }
+  }
+}
