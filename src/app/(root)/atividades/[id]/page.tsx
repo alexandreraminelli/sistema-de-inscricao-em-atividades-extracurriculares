8470a97d85d91d27fa9e6fbaed63a0f671a84158
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-08-05 09:08:38 -0300

    feat(activity-info): card com informações do professor

--- src/app/(root)/atividades/[id]/page.tsx
+++ src/app/(root)/atividades/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
+import { Card } from "@/components/ui/card"
 import { db } from "@/database/drizzle"
-import { activity as activityDb, category as categoryDb } from "@/database/schema"
+import { activity as activityDb, category as categoryDb, teacher as teacherDb, users } from "@/database/schema"
 import { eq } from "drizzle-orm"
 
 /** Parâmetros da rota dinâmica de `ActivityInfoPage`. */
@@ -11,13 +12,11 @@ interface Params {
   }
 }
 /** Página de informações da atividade extracurricular. */
-export default async function ActivityInfoPage({ params }: Params) {
-  // Desestruturar parâmetros da rota dinâmica
-  const { id } = params
-
+export default async function ActivityInfoPage({ params: { id } }: Params) {
   // Obter dados do DB
   const [activity] = await db.select().from(activityDb).where(eq(activityDb.id, id)).limit(1)
   const [category] = await db.select().from(categoryDb).where(eq(categoryDb.id, activity.category)).limit(1)
+  const [teacher] = await db.select().from(users).where(eq(users.id, activity.teacher)).limit(1)
 
   return (
     <div>
@@ -41,18 +40,27 @@ export default async function ActivityInfoPage({ params }: Params) {
       </Breadcrumb>
 
       <main className="my-4">
-        <article className="max-w-5xl mx-auto">
+        {/* Descrição da atividade */}
+        <article className="max-w-5xl mx-auto space-y-6">
           {/* Título */}
           <header>
             <h1 className="my-4 md:my-6 font-semibold text-2xl sm:text-3xl md:text-4xl text-center">{activity.name}</h1>
           </header>
-
           {/* Descrição */}
-          <div className="space-y-4 text-justify text-foreground/90">
+          <section className="space-y-4 text-justify text-foreground/90">
             {activity.description.split("\n").map((paragraph, index) => (
               <p key={index}>{paragraph}</p>
             ))}
-          </div>
+          </section>
+          {/* Dados do aplicador */}
+          <Card className="p-6">
+            <section className="space-y-3">
+              {/* Nome do aplicador */}
+              <h3 className="font-semibold text-xl md:text-2xl">Aplicador: {teacher.name}</h3>
+              {/* Descrição do aplicador */}
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, quod impedit excepturi cupiditate, deserunt aspernatur assumenda eveniet odio itaque est quam dicta voluptate vitae porro perferendis qui enim. Aperiam, numquam.</p>
+            </section>
+          </Card>
         </article>
       </main>
     </div>
