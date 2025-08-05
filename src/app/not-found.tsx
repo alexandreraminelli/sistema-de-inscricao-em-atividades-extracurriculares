a16500810129ffc149e047e92b010b15cd7ae8f3
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-08-05 19:26:23 -0300

    refactor(404): tornar componente NotFound reutilizável

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,51 +1,11 @@
-import Logo from "@/components/custom/Logo"
-import { Button } from "@/components/ui/button"
-import { BookOpenIcon, HomeIcon } from "lucide-react"
-import Image from "next/image"
-import Link from "next/link"
-import AppLayout from "./(root)/layout"
+import NotFoundComponent from "@/components/custom/NotFound"
+import AppLayout from "@/app/(root)/layout"
 
 /** Página de erro 404 (não encontrado). */
 export default function NotFound() {
   return (
     <AppLayout>
-      <div
-        className="flex flex-col items-center 
-        gap-4 px-4 pt-6
-        md:gap-6 md:px-24"
-      >
-        {/* Imagem */}
-        <aside>
-          <Image src="/images/illustrations/404.svg" alt="Ilustração de cachorro olhando triste após comer a página" width={185} height={216} className="max-h-full" />
-        </aside>
-        {/* Título */}
-        <header>
-          <h1 className="font-semibold text-center text-3xl lg:text-4xl">404: Página Não Encontrada</h1>
-        </header>
-        <main className="text-center space-y-2 text-base text-muted-foreground">
-          {/* Explicação */}
-          <p>A página que você está tentando acessar não existe ou não é possível acessá-la no momento.</p>
-          <p>Verifique se o endereço inserido está correto ou tente novamente mais tarde.</p>
-
-          {/* Botões */}
-          <section className="flex flex-row justify-center gap-x-4 gap-y-1.5 flex-wrap">
-            {/* Botão pro início */}
-            <Button size="lg" className="mt-2 md:mt-4 w-fit">
-              <HomeIcon />
-              <Link href="/">Voltar pro início</Link>
-            </Button>
-            {/* Botão de atividades */}
-            <Button size="lg" variant="secondary" className="mt-2 md:mt-4 w-fit">
-              <BookOpenIcon />
-              <Link href="/atividades">Explorar Atividades</Link>
-            </Button>
-          </section>
-        </main>
-        {/* Rodapé */}
-        <footer className="mt-8">
-          <Logo className="h-10 md:h-12" />
-        </footer>
-      </div>
+      <NotFoundComponent title="404: Página Não Encontrada" message={["A página que você está tentando acessar não existe ou não é possível acessá-la no momento.", "Verifique se o endereço inserido está correto ou tente novamente mais tarde."]} />
     </AppLayout>
   )
 }
---
+++ src/components/custom/NotFound.tsx
@@ -0,0 +1,56 @@
+import Logo from "@/components/custom/Logo"
+import { Button } from "@/components/ui/button"
+import { BookOpenIcon, HomeIcon } from "lucide-react"
+import Image from "next/image"
+import Link from "next/link"
+
+/** Props do componente `NotFound`. */
+interface Props {
+  /** Título do erro. */
+  title: string
+  /** Array com parágrafos das mensagens */
+  message: string[]
+}
+/** Componente de página/item não encontrado. */
+export default function NotFound({ title, message }: Props) {
+  return (
+    <div
+      className="flex flex-col items-center 
+        gap-4 px-4 pt-6
+        md:gap-6 md:px-24"
+    >
+      {/* Imagem */}
+      <aside>
+        <Image src="/images/illustrations/404.svg" alt="Ilustração de cachorro olhando triste após comer a página" width={185} height={216} className="max-h-full" />
+      </aside>
+      {/* Título */}
+      <header>
+        <h1 className="font-semibold text-center text-3xl lg:text-4xl">{title}</h1>
+      </header>
+      <main className="text-center space-y-2 text-base text-muted-foreground">
+        {/* Explicação */}
+        {message.map((msg, index) => (
+          <p key={index}>{msg}</p>
+        ))}
+
+        {/* Botões */}
+        <section className="flex flex-row justify-center gap-x-4 gap-y-1.5 flex-wrap">
+          {/* Botão pro início */}
+          <Button size="lg" className="mt-2 md:mt-4 w-fit">
+            <HomeIcon />
+            <Link href="/">Voltar pro início</Link>
+          </Button>
+          {/* Botão de atividades */}
+          <Button size="lg" variant="secondary" className="mt-2 md:mt-4 w-fit">
+            <BookOpenIcon />
+            <Link href="/atividades">Explorar Atividades</Link>
+          </Button>
+        </section>
+      </main>
+      {/* Rodapé */}
+      <footer className="mt-8">
+        <Logo className="h-10 md:h-12" />
+      </footer>
+    </div>
+  )
+}
