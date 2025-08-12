f9029258a16a83f78ee19d5fd83d9d11b2d5477d
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-08-12 08:26:05 -0300

    feat(activity-form): notificação de erro quando nome já estiver em uso

--- src/lib/actions/activity.ts
+++ src/lib/actions/activity.ts
@@ -2,6 +2,7 @@
 
 import { db } from "@/database/drizzle"
 import { activity } from "@/database/schema"
+import { eq } from "drizzle-orm"
 
 /** Retorno de `createActivity`. Se success for `true` acompanha o atributo `data`, senão, acompanha o atributo `message`. */
 type CreateActivityResult =
@@ -16,6 +17,10 @@ type CreateActivityResult =
 /** Função para adicionar uma atividade extracurricular no banco de dados. */
 export async function createActivity(params: typeof activity.$inferInsert): Promise<CreateActivityResult> {
   try {
+    // Verificar se nome já existe
+    const existingActivity = await db.select().from(activity).where(eq(activity.name, params.name)).limit(1)
+    if (existingActivity.length > 0) return { success: false, message: "O nome da atividade já está sendo utilizado.\nEscolha outro nome ou verifique a página de atividades." }
+
     /** Adiciona e recupera a atividade no banco de dados. */
     const newActivity = await db.insert(activity).values(params).returning()
     // Retornar atividade criada
