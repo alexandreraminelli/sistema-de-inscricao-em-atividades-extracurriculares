8e3753f8796ad644b2c50f9ad8f421b4d83eb84f
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-08-12 09:05:49 -0300

    feat(activities): ajuste no estilo do accordion

--- src/app/(root)/atividades/page.tsx
+++ src/app/(root)/atividades/page.tsx
@@ -83,7 +83,7 @@ export default async function ActivityPage() {
                     ))
                   ) : (
                     // Se não houver atividades
-                    <p className="text-muted-foreground">Sem atividades.</p>
+                    <p className="text-muted-foreground ms-2.5">Sem atividades.</p>
                   )}
                 </AccordionContent>
               </AccordionItem>
--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -32,14 +32,7 @@ function AccordionTrigger({
 }: React.ComponentProps<typeof AccordionPrimitive.Trigger>) {
   return (
     <AccordionPrimitive.Header className="flex">
-      <AccordionPrimitive.Trigger
-        data-slot="accordion-trigger"
-        className={cn(
-          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
-          className
-        )}
-        {...props}
-      >
+      <AccordionPrimitive.Trigger data-slot="accordion-trigger" className={cn("focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:cursor-pointer hover:bg-accent/50 px-2.5 focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180", className)} {...props}>
         {children}
         <ChevronDownIcon className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
       </AccordionPrimitive.Trigger>
@@ -55,7 +48,7 @@ function AccordionContent({
   return (
     <AccordionPrimitive.Content
       data-slot="accordion-content"
-      className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm"
+      className="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden mt-2 text-sm"
       {...props}
     >
       <div className={cn("pt-0 pb-4", className)}>{children}</div>
