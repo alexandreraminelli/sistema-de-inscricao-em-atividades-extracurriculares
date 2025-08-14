237c19b1a45fded200ca8f756bc9cb1bcfd54a83
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-08-14 17:54:41 -0300

    refactor(create-activity): melhorar código e renomear tipagem de retorno

--- src/lib/actions/activity.ts
+++ src/lib/actions/activity.ts
@@ -4,8 +4,8 @@ import { db } from "@/database/drizzle"
 import { activity } from "@/database/schema"
 import { eq } from "drizzle-orm"
 
-/** Retorno de `createActivity`. Se success for `true` acompanha o atributo `data`, senão, acompanha o atributo `message`. */
-type CreateActivityResult =
+/** Retorno das funções de operações com atividades. Se success for `true` acompanha o atributo `data`, senão, acompanha o atributo `message`. */
+type ActivityResult =
   | {
       success: true
       data: typeof activity.$inferSelect
@@ -15,18 +15,18 @@ type CreateActivityResult =
       message: string
     }
 /** Função para adicionar uma atividade extracurricular no banco de dados. */
-export async function createActivity(params: typeof activity.$inferInsert): Promise<CreateActivityResult> {
+export async function createActivity(params: typeof activity.$inferInsert): Promise<ActivityResult> {
   try {
     // Verificar se nome já existe
     const existingActivity = await db.select().from(activity).where(eq(activity.name, params.name)).limit(1)
     if (existingActivity.length > 0) return { success: false, message: "O nome da atividade já está sendo utilizado.\nEscolha outro nome ou verifique a página de atividades." }
 
     /** Adiciona e recupera a atividade no banco de dados. */
-    const newActivity = await db.insert(activity).values(params).returning()
+    const [newActivity] = await db.insert(activity).values(params).returning()
     // Retornar atividade criada
     return {
       success: true,
-      data: JSON.parse(JSON.stringify(newActivity[0])),
+      data: JSON.parse(JSON.stringify(newActivity)),
     }
   } catch (error) {
     return { success: false, message: `${error || "Um erro ocorreu ao criar a atividade."}` }
