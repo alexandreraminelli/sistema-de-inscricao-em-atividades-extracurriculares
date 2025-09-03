d4cd182f46b2e9dd45a7febc8b9471b1a7c94bdc
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-09-03 18:22:22 -0300

    feat(theme): adaptar logo ao tema escuro

--- src/components/custom/Logo.tsx
+++ src/components/custom/Logo.tsx
@@ -4,5 +4,12 @@ import Image from "next/image"
 export default function Logo(
   { className, ...props }: Omit<React.ComponentProps<typeof Image>, "src" | "alt"> // aceitar props de Image não definidos
 ) {
-  return <Image src="/logo/logo-inline.svg" alt="Logo do site" width={300} height={82} className={`transition-all ${className}`} {...props} />
+  return (
+    <>
+      {/* Logo light*/}
+      <Image src="/logo/logo-inline.svg" alt="Logo do site" width={300} height={82} className={`transition-all dark:hidden ${className}`} {...props} />
+      {/* Logo darl */}
+      <Image src="/logo/logo-inline-white.svg" alt="Logo do site" width={300} height={82} className={`transition-all hidden dark:block ${className}`} {...props} />
+    </>
+  )
 }
