9b0fdf451a4ab6ce1bdbdbd49eb56971e242162f
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-09-05 11:01:32 -0300

    feat(enrollment): usar ScrollArea para garantir responsividade na lista de horários para inscrição

--- src/components/custom/enrollment/EnrollmentDialogButton.tsx
+++ src/components/custom/enrollment/EnrollmentDialogButton.tsx
@@ -3,6 +3,7 @@
 import ScheduleList from "@/components/custom/schedule/ScheduleList"
 import { Button } from "@/components/ui/button"
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { ScrollArea } from "@/components/ui/scroll-area"
 import { activity, schedule } from "@/database/schema"
 import { ClipboardCheckIcon } from "lucide-react"
 
@@ -12,7 +13,7 @@ interface Props {
   schedules: (typeof schedule.$inferSelect)[]
 }
 
-/** Botão de inscrição em atividade. */
+/** Botão que abre o Dialog para inscrição nas atividades. */
 export default function EnrollmentDialogButton({ activity, schedules }: Props) {
   return (
     <Dialog>
@@ -26,8 +27,10 @@ export default function EnrollmentDialogButton({ activity, schedules }: Props) {
         <DialogHeader>
           <DialogTitle className="leading-7">Realizar Inscrição na atividade {activity.name}</DialogTitle>
           <DialogDescription>Escolha um dos horários abaixo:</DialogDescription>
-          {/* Lista de atividades */}
-          <ScheduleList schedules={schedules} activity={activity} userRole="student" />
+          <ScrollArea className="max-h-[50vh]">
+            {/* Lista de atividades */}
+            <ScheduleList schedules={schedules} activity={activity} userRole="student" />
+          </ScrollArea>
         </DialogHeader>
       </DialogContent>
     </Dialog>
--- src/components/custom/schedule/ScheduleList.tsx
+++ src/components/custom/schedule/ScheduleList.tsx
@@ -8,6 +8,7 @@ interface Props {
   activity: typeof activity.$inferSelect
   schedules: (typeof schedule.$inferSelect)[]
   userRole: UserRole
+  
   classNameInfo?: ClassNameValue
 
   /** Chave para forçar atualização da lista. */
@@ -16,13 +17,13 @@ interface Props {
 /** Lista de horários das atividades. */
 export default function ScheduleList({ activity, userRole, schedules, refreshKey, classNameInfo }: Props) {
   return (
-    <>
+    <section className="w-full flex flex-row md:flex-col flex-wrap gap-4 *:flex-1">
       {schedules.length === 0 ? (
         // Se não houver horários
         <p className="text-muted-foreground text-center">Ainda não há horários definidos.</p>
       ) : (
         schedules.map((s) => <ScheduleInfo key={s.id} activity={activity} schedule={s} userRole={userRole} className={classNameInfo} />)
       )}
-    </>
+    </section>
   )
 }
