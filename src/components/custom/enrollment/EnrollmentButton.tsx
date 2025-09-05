a47630ae902ffdd787965936377df91c6661aa8a
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-09-05 08:56:19 -0300

    chore: tornar o botão de inscrição um icon button

--- src/components/custom/enrollment/EnrollmentButton.tsx
+++ src/components/custom/enrollment/EnrollmentButton.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
 import { schedule } from "@/database/schema"
 import { ClipboardCheckIcon } from "lucide-react"
 
@@ -10,9 +11,14 @@ interface Props {
 /** Botão que realiza a inscrição do aluno em uma atividade. */
 export default function EnrollmentButton({ schedule }: Props) {
   return (
-    <Button size="lg" variant="default">
-      <ClipboardCheckIcon />
-      Inscrever-se
-    </Button>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button size="icon" variant="default">
+          <ClipboardCheckIcon />
+          <span className="sr-only">Inscrever-se</span>
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>Realizar Inscrição</TooltipContent>
+    </Tooltip>
   )
 }
